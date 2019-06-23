c1e48a65c1734c6f59dafbe0bd4bfea39ecb136a
Thinkpad1 <Thinkpad1@DESKTOP-PBKUP8Q>
2019-06-22 23:15:15 -0700

    New class for Task

---
+++ HRMS/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRMS
+{
+    class Task
+    {
+        public int TaskId { get; set; }
+        public string Description { get; set; }
+    }
+}
