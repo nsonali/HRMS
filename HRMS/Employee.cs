1a405a7d0c206315d9dea6d02a9c8c41d0343e53
Thinkpad1 <Thinkpad1@DESKTOP-PBKUP8Q>
2019-06-22 22:09:26 -0700

    commit

---
+++ HRMS/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HRMS
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string EmailId { get; set; }
+        public int Phone { get; set; }
+        public string Role { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Joiningdate { get; set; } 
+
+
+    }
+
+}
+
+   
---
+++ HRMS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HRMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
