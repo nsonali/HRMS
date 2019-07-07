43e246dde9a2000a5d7a0e2de8642bafc6c4e43b
Thinkpad1 <Thinkpad1@DESKTOP-PBKUP8Q>
2019-07-06 17:05:06 -0700

    added enum for role and "Add" method

--- HRMS/Employee.cs
+++ HRMS/Employee.cs
@@ -4,6 +4,13 @@ using System.Text;
 
 namespace HRMS
 {
+    enum Role
+    {
+        Administrator,
+        ProjectManager,
+        SDET,
+        STE
+    }
     //Employee class has all the details about employee
     class Employee
     {
@@ -12,11 +19,18 @@ namespace HRMS
         public string City { get; set; }
         public string EmailId { get; set; }
         public int Phone { get; set; }
-        public string Role { get; set; }
+        public Role EmpRole { get; set; }
         public decimal Salary { get; set; }
         public DateTime Joiningdate { get; set; } 
 
-
+        //Add employee
+        public void Add(string name)
+        {
+            Employee employee = new Employee();
+            employee.Name = name;
+            Console.WriteLine("new employee added : " + employee.Name);
+            //save this info to Database
+        }
     }
 
 }
--- HRMS/Program.cs
+++ HRMS/Program.cs
@@ -6,8 +6,41 @@ namespace HRMS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            
+            Console.WriteLine("Welcome to HRMS");
+            Console.WriteLine("*****************");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. New Employee Form");
+            Console.WriteLine("2. List all employee's");
+
+            var choice = Console.ReadLine();
+            switch(choice)
+            {
+                case "0":
+                    Console.WriteLine("Thank you for visiting");
+                    return;
+              
+                case "1":
+                    Console.WriteLine("Employee name: ");
+                    var empname = Console.ReadLine();
+                    //create new instance of employee class
+                    Employee emp = new Employee();
+                    //pass user provided name to Add method
+                    emp.Add(empname);
+
+                    break;
+                case "2":
+                    break;
+                default:
+                    break;
+
+            }
+
         }
+
+
+
+
+            
+        
     }
 }
